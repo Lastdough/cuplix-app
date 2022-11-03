4b849722b3678906153aa952045b5b03a5334286
abdur <abdurrahman.abdur02@gmail.com>
2022-11-03 21:02:54 +0700

    new svg

---
+++ app/Http/Livewire/Welcome/Image.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Welcome;
+
+use Livewire\Component;
+
+class Image extends Component
+{
+    public function render()
+    {
+        return view('livewire.welcome.image');
+    }
+}
